ad7b8e5dca889d27cf4e0b97f39200b634d49303
Joshua Jacobs <joshuajacobs14@gmail.com>
2015-09-01 09:50:32 -0600

    no message

--- DGM 1600 - Josh Jacobs/Assets/Variable and Functions.cs
+++ DGM 1600 - Josh Jacobs/Assets/Variable and Functions.cs
@@ -3,11 +3,23 @@ using System.Collections;
 
 public class VariableandFunctions : MonoBehaviour {
 
+	int myNumber = 5;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		myNumber = MultiplyByTwo (myNumber);
+		Debug.Log (myNumber);
+
 	}
-	
+
+	int MultiplyByTwo (int number)
+	{
+		int doubleN;
+		doubleN = number * 2;
+		return doubleN;
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
