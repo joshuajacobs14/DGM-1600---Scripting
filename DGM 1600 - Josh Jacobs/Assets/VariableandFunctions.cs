d6c7f2fa94e5429baa1f143188a5325ddb807943
Joshua Jacobs <joshuajacobs14@gmail.com>
2015-09-15 09:15:45 -0600

    no message

---
+++ DGM 1600 - Josh Jacobs/Assets/VariableandFunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariableandFunctions : MonoBehaviour {
+	
+	int myNumber = 5;
+	
+	// Use this for initialization
+	void Start () {
+		
+		myNumber = DivideByTwo (myNumber);
+		Debug.Log (myNumber);
+		
+	}
+	
+	int DivideByTwo (int number)
+	{
+		int doubleN;
+		doubleN = number / 2;
+		return doubleN;
+	}
+}
