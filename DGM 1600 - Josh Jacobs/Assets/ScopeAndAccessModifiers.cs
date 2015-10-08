75178d7fbc7a7cc8ba4ad79e61b021f900e01f42
Joshua Jacobs <joshuajacobs14@gmail.com>
2015-10-08 09:02:54 -0600

    adding scope and access modifiers

---
+++ DGM 1600 - Josh Jacobs/Assets/ScopeAndAccessModifiers.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAndAccessModifiers : MonoBehaviour {
+
+	public int alpha = 5;
+	public int donuts = 12;
+	private int muffins = 6;
+	private int danish = 3;
+
+
+	private SecondScript otherClass;
+
+
+	void start ()
+	{
+		alpha = 29;
+
+		otherClass = new SecondScript();
+		otherClass.FruitCounter(alpha, otherClass.cherry);
+		
+	}
+
+
+	void Example (int cakes, int fritters)
+	{
+		int answer;
+		answer = cakes * fritters * alpha;
+		Debug.Log (answer);
+	}
+
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			alpha += muffins;
+			alpha += danish;
+		}
+
+
+		Debug.Log ("Alpha is set to: " + alpha);
+
+
+	}
+	/*
+	void countPastries() 
+	{
+		int pastries = donuts + muffins;
+		
+		Debug.Log ("There are " + donuts + 
+		           " donuts and " + muffins + " muffins.");
+		Debug.Log("There are " + pastries + " pastries total");
+	
+	}
+	*/
+
+
+}
---
+++ DGM 1600 - Josh Jacobs/Assets/SecondScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondScript : MonoBehaviour {
+
+	public int cherry;
+	public int grapes;
+
+	private int carrot;
+	private int pumpkin;
+
+
+	public void FruitCounter (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log ("There are " + answer + " fruit.");
+	}
+
+	public void VeggieCounter (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log ("There are " + answer + " veggies.");
+	}
+	
+}
